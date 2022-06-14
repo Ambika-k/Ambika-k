eaca770303d57cc1ddef864a0eb02e4e58f2eca7
Ambika-k <ambikakumpatlas44@gmail.com>
2022-06-14 16:26:58 +0530

    updated

--- springbootDemo/src/main/java/com/zensar/springbootDemo/Application.java
+++ springbootDemo/src/main/java/com/zensar/springbootDemo/Application.java
@@ -13,7 +13,6 @@ public class Application implements ApplicationRunner{
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 		System.out.println("Applicationrunner"+Arrays.toString(args.getSourceArgs()));
-		
 	}
 
 }
