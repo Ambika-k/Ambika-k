140bc286cfa5b12ba2825b82165ed5a282b66de8
Ambika-k <ambikakumpatlas44@gmail.com>
2022-07-01 11:59:39 +0530

    "War file removed"

--- springbootDemo/src/main/java/com/zensar/springbootDemo/SpringbootDemoApplication.java
+++ springbootDemo/src/main/java/com/zensar/springbootDemo/SpringbootDemoApplication.java
@@ -3,6 +3,8 @@ package com.zensar.springbootDemo;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -11,7 +13,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 @SpringBootApplication
 @ComponentScan(basePackages = {"test","com.zensar"})
 @OpenAPIDefinition
-public class SpringbootDemoApplication {
+public class SpringbootDemoApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDemoApplication.class, args);
@@ -19,6 +21,11 @@ public class SpringbootDemoApplication {
 	@Bean
 	public ModelMapper getModelMapper() {
 		return new ModelMapper();
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return super.configure(builder);
 	}
 	
 	
