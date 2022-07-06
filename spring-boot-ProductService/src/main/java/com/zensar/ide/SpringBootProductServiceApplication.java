6446ad87aa5ffa7dd7a3882ffdb1564db077e62d
Ambika-k <ambikakumpatlas44@gmail.com>
2022-07-06 20:30:06 +0530

    Oauth2 implemented

--- spring-boot-ProductService/src/main/java/com/zensar/ide/SpringBootProductServiceApplication.java
+++ spring-boot-ProductService/src/main/java/com/zensar/ide/SpringBootProductServiceApplication.java
@@ -3,12 +3,14 @@ package com.zensar.ide;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 
@@ -19,6 +21,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 @OpenAPIDefinition
 @EnableEurekaClient
 @EnableFeignClients
+@EnableOAuth2Sso
 public class SpringBootProductServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootProductServiceApplication.class, args);
